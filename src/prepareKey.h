e85fec64cf45ecbae1170cfcf485e1acca8810ef
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2026-05-09 18:52:33 +0500

    updated signature

--- src/prepareKey.h
+++ src/prepareKey.h
@@ -1,4 +1,7 @@
+#pragma once
+
 #include <linux/input.h>
 #include <stdbool.h>
 
-char prepareKey(__u16 code, bool isShiftAcitve, bool isCtrlActive);
+char *prepareKey(__u16 code, __s32 value, bool *isShiftActive, bool *isCtrlActive,
+                 bool *isCapsLockActive);
